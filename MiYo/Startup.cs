36cf9d93d866df323de2b8bc97d39dd7b0c9477c
lavieennoir <dmitriykhiznyak@gmail.com>
2018-03-25 22:05:01 +0300

    Add project files.

---
+++ MiYo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiYo.Startup))]
+namespace MiYo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
